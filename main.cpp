9858e8f381c38c25eb0ae2c8cfe7f6260469624b
shayan <smartgamer56th@gmail.com>
2025-12-20 17:13:27 +0500

    Made the interactive main part

--- main.cpp
+++ main.cpp
@@ -3,22 +3,67 @@
 using namespace std;
 int main() {
     TaskManager tm;
-    tm.addTask(101, "Fix Login", "Fix crash on startup", 10, 30, 2);
-    tm.addTask(102, "Do laundry", "Finish all the clothes in my room", 5, 60, 5);
-    tm.addTask(103, "Play Games", "RDR2", 1, 60, 10);
-    tm.displayAll();
+    int choice;
 
-    tm.removeTask(101);
-    tm.displayAll();
-    tm.History();
-
-    cout << "\n--- Testing Undo ---" << endl;
-    tm.undo();      // Should restore the taskjust finished
-    tm.displayAll(); //it appears in the list again
-
-    return 0;
+    int id, priority, duration, deadline;
+    string name, description;
+    cout<<"====================================="<<endl;
+    cout<<"  PRIORITY-BASED TASK SCHEDULER SYSTEM"<<endl;
+    cout<<"==================================="<<endl;
+    while (true) {
+        cout<<"Main Menu"<<endl;
+        cout<<"1. Add Task"<<endl;
+        cout<<"2. Completed Task"<<endl;
+        cout<<"3. Show All Tasks"<<endl;
+        cout<<"4. Recommended Task"<<endl;
+        cout<<"5. Undo Last Completion"<<endl;
+        cout<<"6. View History"<<endl;
+        cout<<"0. Exit"<<endl;
+        cin>>choice;
+        cin.ignore();
+        switch (choice) {
+            case 1:
+                cout<<"Enter ID: "<<endl;
+                cin>>id;
+                cin.ignore();
+                cout<<"Enter Name: "<<endl;
+                getline(cin,name);
+                cout<<"Enter Description: "<<endl;
+                getline(cin,description);
+                cout<<"Enter Priority: "<<endl;
+                cin>>priority;
+                cout<<"Enter Duration: "<<endl;
+                cin>>duration;
+                cout<<"Enter Deadline: "<<endl;
+                cin>>deadline;
+                tm.addTask(id, name, description, priority, duration, deadline);
+                break;
+            case 2:
+                cout<<"Enter Task ID to remove: "<<endl;
+                cin>>id;
+                tm.removeTask(id);
+                break;
+            case 3:
+                tm.displayAll();
+                break;
+            case 4:
+                tm.getNextTask();
+                break;
+            case 5:
+                tm.undo();
+                break;
+            case 6:
+                tm.History();
+                break;
+            case 0:
+                cout<<"Exiting..."<<endl;
+                return 0;
+            default:
+                cout<<"Invalid choice"<<endl;
+        }
+    }
 }
 // TIP See CLion help at <a
 // href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
 //  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
+//  'Help | Learn IDE Features' from the main menu
